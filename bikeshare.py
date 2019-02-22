9d5e745b711b9f449f9a037b9707c66b315159eb
SanemKocak <sanemkocak@gmail.com>
2019-02-22 14:56:29 -0800

    Removed unused numpy import statement and added author in bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -1,8 +1,8 @@
 
 import time
 import pandas as pd
-import numpy as np
 
+#author: Sanem Kocak Balkan
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
